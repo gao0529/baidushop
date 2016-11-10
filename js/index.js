d80cf38b80dc70879a8f56fcf8cbb1cfa050bc12
gaopeng <413886212@qq.com>
2016-11-10 08:58:21 +0800

    shop
    shop

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
 $(function(){
-
-	// 图片放大
-	// var img=$(".banner img");
-	// var div=$('.banner img-box')
-	// div.hover(function(){
- //        img.eq(div.index(this)).animate({},500)
-	// },function(){
-	// 	img.eq(div.index(this)).animate({},500)
-	// })
-	
-	
 	// 图片左移
 	var imgs=$('.list-right li img');
 	var list=$('.list-right li');
@@ -20,6 +9,57 @@ $(function(){
 	})
 
 
+	//banner轮播
+    var imag_box=$('.images');
+    var banner=$('.banner-left');
+    var b=$('.image-list');
+    var t=setInterval(move,3000);
+    var i=0;
+    function move(){
+        var x=$('.image-list').width();
+    	i++;
+    	if (i==b.length) {
+    		i=0;
+    	}
+    	imag_box.animate({left:x*-i},1000)
+        b_btn.removeClass('hot').eq(i).addClass('hot');
+    }
+    
+    var b_btn=$('.banner-btn');
+    b_btn.eq(0).addClass('hot');
+    b_btn.click(function(){
+        clearInterval(t);
+        i=b_btn.index(this);
+        var x=$('.image-list').width();
+        b_btn.removeClass('hot').eq(i).addClass('hot');
+        imag_box.animate({left:x*-i},1000,function(){
+            var t=setInterval(move,3000);
+        });
+    })
+    var b_left=$('.btn-left');
+    var b_right=$('.btn-right');
+    b_right.click(function(){
+        var x=$('.image-list').width();
+       clearInterval(t)
+            i++
+            if (i==b.length) {
+                i=0;
+            };
+            imag_box.animate({left:x*-i},1000)
+            b_btn.removeClass('hot').eq(i).addClass('hot');
+            t=setInterval(move,1500)
+    })
+    b_left.click(function(){
+        var x=$('.image-list').width();
+         clearInterval(t)
+            i--
+            if (i==-1) {
+                i=b.length-1;
+            };
+            imag_box.animate({left:x*-i},1000)
+            b_btn.removeClass('hot').eq(i).addClass('hot');
+            t=setInterval(move,1500)
+    })
 
 
 })
